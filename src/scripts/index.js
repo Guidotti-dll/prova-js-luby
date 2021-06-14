e0baccf4bb7a35084bd89b73e3df010c72d9b786
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-06-14 17:11:58 -0300

    feat: getGamesInfos function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,32 @@
 (function () {
-  'use strict'
-}())
+  'use strict';
+
+    const app = (function() {
+      return {
+        init : function init() {
+          app.getGamesInfo();
+        },
+
+        getGamesInfo: function getGamesInfo() {
+          const ajax = new XMLHttpRequest();
+          ajax.open('GET', '../../games.json');
+          ajax.send();
+          ajax.addEventListener('readystatechange', function () {
+            if(app.isReady.call(this)){
+              app.renderGames(JSON.parse(ajax.responseText));
+            }
+          })
+        },
+
+        renderGames: function renderGames(games) {
+          console.log(games);
+        },
+
+        isReady: function isReady() {
+          return this.readyState === 4 && this.status === 200;
+        },
+      }
+    }())
+
+    app.init();
+})();
